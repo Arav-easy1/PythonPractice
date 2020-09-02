fe628485a4ed7311beb593136c6661884b9bb95e
EASY_ARAV <wg0418@gmail.com>
2020-09-02 18:49:07 +0900

    기본 입출력

--- untitled-1.py
+++ untitled-1.py
@@ -1,6 +1,9 @@
 # -*- Encoding: utf-8 -*-
-a = 2
-# 00000010 이진법쓰 시프트연산 <<시프트연산 00000100
-print(a << 3)
-b = 10
-print(a ** b)       #거듭제곱
+a = input("데이터를 넣어주세요 >>> ").split(' ')
+x = int(a[0])
+y = int(a[1])
+# x와 y에 각각 특정한 수가 담겨 있게 됨.
+print("x + y = ", x + y)
+print("x - y = ", x - y)
+print("x * y = ", x * y)
+print("x / y = ", x / y)
