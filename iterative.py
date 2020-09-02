dc32dd1bfc5717dce025a7111c8dca8edf21567f
EASY_ARAV <wg0418@gmail.com>
2020-09-02 22:06:41 +0900

    반복문

---
+++ iterative.py
@@ -0,0 +1,12 @@
+# 반복문: 조건에 부합하는 한 특정한 명령어를 반복
+# 숫자 범위 표현: range(시작, 끝)
+# continue: continue를 만났을 때 더이상 명령어를 실행하지 않고 다음 반복을 진행한다
+# break: break를 만나면 반복문을 벗어난다
+i = 0
+sum = 0
+while i < 5:
+    i = i + 1
+    if i % 2 == 1:
+        continue
+    sum = sum + i
+print("합계: ", sum)
