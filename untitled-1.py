4cd9d66af129d00ac846886a45294dfdf0eca3e9
EASY_ARAV <wg0418@gmail.com>
2020-09-02 18:56:28 +0900

    알고리즘 풀어보기

--- untitled-1.py
+++ untitled-1.py
@@ -1,9 +1,5 @@
 # -*- Encoding: utf-8 -*-
-a = input("데이터를 넣어주세요 >>> ").split(' ')
+a = input().split(' ')
 x = int(a[0])
 y = int(a[1])
-# x와 y에 각각 특정한 수가 담겨 있게 됨.
-print("x + y = ", x + y)
-print("x - y = ", x - y)
-print("x * y = ", x * y)
-print("x / y = ", x / y)
+print(x + y)
