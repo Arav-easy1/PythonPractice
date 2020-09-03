05621d50ff6c55a7565868dc7934074ef5c321ce
EASY_ARAV <wg0418@gmail.com>
2020-09-04 00:43:34 +0900

    파이썬의 대표적 내장함수

---
+++ built_in_functions.py
@@ -0,0 +1,26 @@
+# input(): 사용자로부터 콘솔로 입력을 받는 함수
+# int(): 정수 자료형으로 변환
+    # user_input = input('정수를 입력하세요: ')
+    # print("제곱: ", int(user_input) ** 2)
+# float(): 문자열, 정수 등의 자료형을 실수형으로 변환
+    # a = 10
+    # print(float(a))
+# max(), min(): 시퀀스 자료형에 포함되어 있는 원소 중 최대값 혹은 최소값
+    # list = [5, 6, 4, 75, 4, 6, 3, 65]
+    # print(max(list))
+    # print(min(list))
+# bin(), hex(): 10진수 -> 2진수 변환, 10진수 -> 16진수 변환
+    # print(bin(128))
+    # print(hex(230))
+    # print(int('0xe6', 16))
+# round(): 반올림을 수행
+    # print(round(123.768, 2))
+# type(): 자료형의 종류
+    # int = 1
+    # str = '문자열'
+    # list = [1, 3, 4]
+    # dict = {'apple': '사과'}
+    # print(type(int))
+    # print(type(str))
+    # print(type(list))
+    # print(type(dict))
